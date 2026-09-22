b977f86ba5fee90d9d3b4a6c7efbe43a6fbde777
Bauge Angel <baugeangel@gmail.com>
2026-09-22 14:24:36 +0200

    small change

--- src/integration_method.cpp
+++ src/integration_method.cpp
@@ -1,1 +1,27 @@
 #include "integration_method.hpp"
+
+namespace {
+
+template <typename Scalar>
+void load_integration_methods()
+{
+    [[maybe_unused]] integration_method::QuadParameters<Scalar>
+        quad_parameters{};
+
+    using QuanticsParameters =
+        integration_method::QuanticsFitParameters<Scalar>;
+    using RunnerParameters =
+        typename QuanticsParameters::Quantics::RunnerParam;
+
+    [[maybe_unused]] QuanticsParameters quantics_parameters{
+        RunnerParameters{1, 1, 1}, {}};
+}
+
+} // namespace
+
+int main()
+{
+    load_integration_methods<double>();
+    load_integration_methods<dd_128>();
+    return 0;
+}
